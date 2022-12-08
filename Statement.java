22fe097a091cbae2de9c57b1de3f991c28eddeec
Jonnas Freire <jonnas.freire17@gmail.com>
2022-12-08 15:51:27 +0000

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,9 @@
-public class Statement {
+import java.util.Enumeration;
 
+public abstract class Statement {
+	public abstract String statement(Enumeration<Rental> rentals, Customer customer);
+
+	public String value(Customer aCustomer) {
+      return this.statement(aCustomer.getRentals(), aCustomer);
+   }
 }
