876ef909b78b32c375dd160e0c84fa346b7995dc
Jonnas Freire <jonnas.freire17@gmail.com>
2022-12-08 13:57:53 +0000

    Commit 7

--- Customer.java
+++ Customer.java
@@ -25,18 +25,35 @@ public class Customer {
 	  
      while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement();
-		frequentRenterPoints += each.getFrequentRenterPoints();
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
             String.valueOf(each.getCharge()) + "\n";
-        totalAmount += each.getCharge();
-
      }
      //add footer lines
-     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-     result += "You earned " + String.valueOf(frequentRenterPoints) +
+     result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+     result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
              " frequent renter points";
      return result;
+   }
+
+	private double getTotalCharge() {
+      double result = 0;
+      Enumeration<Rental> rentals = _rentals.elements();
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         result += each.getCharge();
+         }
+         return result;
+   }
+
+   private int getTotalFrequentRenterPoints(){
+      int result = 0;
+      Enumeration<Rental> rentals = _rentals.elements();
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         result += each.getFrequentRenterPoints();
+      }
+      return result;
    }
 }
