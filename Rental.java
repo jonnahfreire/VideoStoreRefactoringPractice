a56056153939dec6d88c7b00c8c6fccea6e79a95
Jonnas Freire <jonnas.freire17@gmail.com>
2022-12-08 14:13:51 +0000

    Commit 10

--- Rental.java
+++ Rental.java
@@ -1,34 +1,34 @@
 public class Rental {
 
-   private Movie _movie;
-   private int _daysRented;
+	private Movie _movie;
+	private int _daysRented;
 
-   public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-   }
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
 
-   public int getDaysRented() {
-      return _daysRented;
-   }
+	public int getDaysRented() {
+		return _daysRented;
+	}
 
-   public Movie getMovie() {
-      return _movie;
-   }
+	public Movie getMovie() {
+		return _movie;
+	}
 
-	public int getFrequentRenterPoints() { 
-    // add frequent renter points
+	public int getFrequentRenterPoints() {
+		// add frequent renter points
 		int frequentRenterPoints = 0;
-        
-        // add bonus for a two day new release rental
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-			getDaysRented() > 1) frequentRenterPoints ++;
+
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+			frequentRenterPoints++;
 
 		return frequentRenterPoints;
-   }
+	}
 
-	//determine amounts for each line
+	// determine amounts for each line
 	public double getCharge() {
-        return _movie.getCharge(_daysRented);
+		return _movie.getCharge(_daysRented);
 	}
 }
